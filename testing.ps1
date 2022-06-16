fa18a4f6f7993a810625d87c4e865118efd3b7c0
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-06-16 08:03:57 -0500

    more testing

--- testing.ps1
+++ testing.ps1
@@ -1,5 +1,32 @@
-$dataFromCSV = Import-Csv -Path /username_extracted_final.csv
+# ---------------------------------------------------------------------------
+#
+# This script requires a computer name.  It will return the computer's 
+# currently logged-in user's default printer.
+#
+# ---------------------------------------------------------------------------
 
-foreach ($dataLine in $dataFromCSV) {
-    Set-ADUser -Identity $dataLine.UserName_Extracted -OfficePhone $dataLine.Label
-}
+# Set the variable below to choose your computer
+$Computer = "computer_name"
+
+
+# get the logged-in user of the specified computer
+$user = Get-WmiObject –ComputerName $computer –Class Win32_ComputerSystem | Select-Object UserName
+
+# get that user's AD object
+$AdObj = New-Object System.Security.Principal.NTAccount($user.UserName)
+
+# get the SID for the user's AD Object 
+$strSID = $AdObj.Translate([System.Security.Principal.SecurityIdentifier])
+
+# get a handle to the "USERS" hive on the computer
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("Users", $Computer)
+
+# get a handle to the current user's USERS Registry key where the default printer value lives
+$regKey = $reg.OpenSubKey("$strSID\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Windows")
+
+# read and show the new value from the Registry for verification
+$regValue = $regKey.GetValue("Device")
+write-output $regValue
+write-output " "
+write-output " "
+[void](Read-Host 'Press Enter to continue…')
